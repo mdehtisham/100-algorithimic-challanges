5c6c59677c6c58640cdce83aca4d54fa176b4066
mdehtisham <mefxe01@gmail.com>
2021-02-24 22:24:52 +0530

    solved adjacetElementsProduct problem and added the solution

---
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -0,0 +1,10 @@
+function adjacentElementProduct(arr: number[]): number{
+  let result: number = 0
+  for(let i=1; i<arr.length; i++){
+    if(result < arr[i]*arr[i-1]) result = arr[i]*arr[i-1]
+    else continue
+  }
+  return result
+  }
+  
+  console.log(adjacentElementProduct([3, 6, -2, -5, 7, 3]))
