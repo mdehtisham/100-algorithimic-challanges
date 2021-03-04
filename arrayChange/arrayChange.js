da31f79635789a13427b0da4ed200d1017647314
mdehtisham <mefxe01@gmail.com>
2021-03-04 09:58:42 +0530

    improved time complexity of arrayChange problem

--- arrayChange/arrayChange.js
+++ arrayChange/arrayChange.js
@@ -1,3 +1,4 @@
+// solution 1 -> time complexity O(n^2)
 function arrayChange(arr){
   let count = 0;
 
@@ -10,6 +11,24 @@ function arrayChange(arr){
   return count
 }
 
-console.log(arrayChange([1,1,1])) // 3
-console.log(arrayChange([1,2,1])) // 2
-console.log(arrayChange([1,1,3])) // 1
+// console.log(arrayChange([1,1,1])) // 3
+// console.log(arrayChange([1,2,1])) // 2
+// console.log(arrayChange([1,1,3])) // 1
+
+
+// solution 2 -> time complexity O(n)
+function arrayChange2(arr){
+  let count = 0
+  for(let i=0; i<arr.length; i++){
+    if(arr[i]>= arr[i+1]){
+      const difference = (arr[i] + 1) - arr[i+1]
+      arr[i+1] = arr[i] + 1
+      count += difference
+    }
+  }
+  return count
+}
+
+console.log(arrayChange2([1,1,1])) // 3
+console.log(arrayChange2([1,2,1])) // 2
+console.log(arrayChange2([1,1,3])) // 1
